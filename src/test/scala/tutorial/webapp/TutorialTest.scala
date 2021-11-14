615be60fb79e5423624946e53fc9884e45f7e444
joe <joe.chimienti@eventdynamic.com>
2021-11-14 10:16:09 -0500

    upate specs

--- src/test/scala/tutorial/webapp/TutorialTest.scala
+++ src/test/scala/tutorial/webapp/TutorialTest.scala
@@ -15,21 +15,21 @@ object TutorialTest extends TestSuite {
 
   def tests = Tests {
     test("HelloWorld") {
-      assert(document.querySelectorAll("p").count(_.textContent == "Hello World") == 1)
+      assert(document.querySelectorAll("h1").count(_.textContent == "LNURL Codec") == 1)
     }
 
-    test("ButtonClick") {
-      def messageCount =
-        document.querySelectorAll("p").count(_.textContent == "You clicked the button!")
-
-      val button = document.querySelector("button").asInstanceOf[dom.html.Button]
-      assert(button != null && button.textContent == "Click me!")
-      assert(messageCount == 0)
-
-      for (c <- 1 to 5) {
-        button.click()
-        assert(messageCount == c)
-      }
-    }
+//    test("ButtonClick") {
+//      def messageCount =
+//        document.querySelectorAll("p").count(_.textContent == "You clicked the button!")
+//
+//      val button = document.querySelector("button").asInstanceOf[dom.html.Button]
+//      assert(button != null && button.textContent == "Click me!")
+//      assert(messageCount == 0)
+//
+//      for (c <- 1 to 5) {
+//        button.click()
+//        assert(messageCount == c)
+//      }
+//    }
   }
 }
